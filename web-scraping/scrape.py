038d29600496acb7eb981eab9c48b0c65455aa1a
jerinjaimon <jerinjaimon@gmail.com>
2022-03-30 13:13:53 +0530

    feat: Add flask server to display news in webpage

--- web-scraping/scrape.py
+++ web-scraping/scrape.py
@@ -1,32 +1,41 @@
-"""Fetch stock calls and market news."""
+"""Harvest stock calls and market news."""
 import requests
 from bs4 import BeautifulSoup
-# import pprint
+from flask import Flask, render_template
 
-res1 = requests.get('https://www.moneycontrol.com/news/business/stocks/')
-res2 = requests.get('https://www.moneycontrol.com/news/business/stocks/page-2')
-soup1 = BeautifulSoup(res1.text, 'html.parser')
-soup2 = BeautifulSoup(res2.text, 'html.parser')
-
-# heads up! .storylink changed to .titlelink
-link1 = soup1.select('#cagetory')
-link2 = soup2.select('#cagetory')
-links = link1 + link2
+app = Flask(__name__)
 
 
-def fetch_buy_calls(links):
-    """Fetch market feeds."""
-    calls = []
-    for item in links:
-        level1 = item.select('.clearfix')
-        for element in level1:
-            # sub_element = element.h2
-            level2 = element.a
-            calls.append(level2.get('title'))
-    return calls
+def get_stock_reco():
+    """Stock reco data extraction using Web Scraping."""
+    res1 = requests.get('https://www.moneycontrol.com/news/business/stocks/')
+    res2 = requests.get(
+        'https://www.moneycontrol.com/news/business/stocks/page-2')
+    soup1 = BeautifulSoup(res1.text, 'html.parser')
+    soup2 = BeautifulSoup(res2.text, 'html.parser')
+
+    # heads up! .storylink changed to .titlelink
+    link1 = soup1.select('#cagetory')
+    link2 = soup2.select('#cagetory')
+    links = link1 + link2
+
+    def fetch_market_news(links):
+        """Fetch market feeds."""
+        news = []
+        for item in links:
+            level1 = item.select('.clearfix')
+            for element in level1:
+                # sub_element = element.h2
+                level2 = element.a
+                news.append(level2.get('title'))
+        return news
+
+    market_news = fetch_market_news(links)
+    return market_news
 
 
-# pprint.pprint
-calls = fetch_buy_calls(links)
-for call in calls:
-    print(call)
+@ app.route("/")
+def calls():
+    """Server code."""
+    market_feed = get_stock_reco()
+    return render_template('index.html', calls=market_feed)
