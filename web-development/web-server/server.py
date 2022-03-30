5243374910df0babe522566bc79cdca9af31aefe
jerinjaimon <jerinjaimon@gmail.com>
2022-03-30 12:13:58 +0530

    Style fix

--- web-development/web-server/server.py
+++ web-development/web-server/server.py
@@ -1,3 +1,4 @@
+"""Flask server code sample."""
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
@@ -10,34 +11,34 @@ def home_page():
 
 
 @app.route("/about.html")
-def about_page(username="Jerin", id=101):
+def about_page(username="Jerin", idx=101):
     """About page."""
-    return render_template('about.html', name=username, identity=id)
+    return render_template('about.html', name=username, identity=idx)
 
 
 @app.route("/<username>/<int:id>")
-def id_page(username=None, id=None):
+def id_page(username=None, idx=None):
     """About page with dynamic data from url."""
-    return render_template('about.html', name=username, identity=id)
+    return render_template('about.html', name=username, identity=idx)
 
 
 @app.route("/registration.html")
 def reg_page():
-    """Registration Page"""
+    """Registration Page."""
     return render_template('registration.html')
 
 
 @app.route("/register", methods=['POST', 'GET'])
-def registration(username=None, id=None):
-    """Registration page."""
+def registration(username=None, idx=None):
+    """Get form data."""
     if request.method == 'POST':
         data = request.form.to_dict()
         print(data)
         username = data['name']
-        id = data['id']
+        idx = data['id']
         response = "Registration Sucess"
         return render_template(
-            'about.html', name=username, identity=id, response=response)
+            'about.html', name=username, identity=idx, response=response)
     else:
         response = "Registration Failed"
     return response
